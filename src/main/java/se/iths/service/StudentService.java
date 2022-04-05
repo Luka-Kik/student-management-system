a4447a2e12ece0b2d3db4fa20903bf2d28c3b664
penzija <luka.kik@iths.se>
2022-04-05 10:47:59 +0200

    First commit

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,24 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
 public class StudentService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void createStudent(Student student) {
+        entityManager.persist(student);
+    }
+
+    public void updateStudent(Student student) {
+        entityManager.merge(student);
+    }
 
 
 }
